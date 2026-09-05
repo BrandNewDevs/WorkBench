167bf42c62a189b0aea11194594cf05e75652892
AkshatSingh4477 <akshatsingh4477@gmail.com>
2026-09-05 17:42:03 +0530

    feat(backend1): last fix revoke

--- apps/web/src/components/Navbar.tsx
+++ apps/web/src/components/Navbar.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 export default function Navbar({ isFeaturesPage, isAboutPage }: { isFeaturesPage: boolean; isAboutPage: boolean }) {
   const isPage = isFeaturesPage || isAboutPage;
   return (
